de18c09f95c5ab722be4c828270066a80152e834
aldiyaramanzhol <aldi12062006@gmail.com>
2025-03-30 23:12:48 +0500

    Finding the minimum element in an array (Recursion)

--- src/problem10.java
+++ src/problem10.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class problem10 {
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(gcd(a, b));
+    }
 }
+
+
+//To find the minimum element of an array, compare its first element with the minimum value in the rest of the array. If the array consists of one element, it is the minimum. The algorithm calls the function n times (where n is the size of the array), so its complexity is O(n).
